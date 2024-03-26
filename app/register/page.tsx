9732932d2bc76af792b789fbe27a38d534c8882e
Sophia Castillo <sphcastillo@gmail.com>
2024-03-26 14:54:36 -0700

    see new changes to fix error

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,7 +9,7 @@ async function Register() {
             <div className="mx-auto max-w-7xl px-6 pb-12 pt-16 text-white text-center lg:px-8">
                 <div className="mx-auto max-w-4xl">
                 <p className="mt-2 text-4xl font-bold tracking-tight sm:text-5xl">
-                    Lets handle your Membership {session?.user?.name?.split(" ")?.[0]}!
+                    Lets handle your Membership, {session?.user?.name?.split(" ")?.[0]}!
                 </p>
                 </div>
                 <div className="relative">
@@ -34,7 +34,7 @@ async function Register() {
                 </div>
             </div>
 
-        <PricingCards redirect={false} />
+            <PricingCards redirect={false}/>
         </div>
     )
 }
--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
-import { NextAuthOptions } from 'next-auth';
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { NextAuthOptions, getServerSession } from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
+import { adminDb } from './firebase-admin';
+
 
 export const authOptions: NextAuthOptions = {
     providers: [
@@ -8,7 +11,18 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    // callbacks: {
+    //     // appending user id to the user
+    //     jwt: async({ user, token }) => {
+    //         if (user) {
+    //             token.sub = user.id;
+    //         }
+    //         return token;
+    //     }
+    // },
     session: {
         strategy: 'jwt',
-    }
+    },
+    adapter: FirestoreAdapter(adminDb),
 } satisfies NextAuthOptions;
+
